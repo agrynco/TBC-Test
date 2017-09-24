3068d223b1579a572ccbfdb1293bf18016a0bc52
agrynco <agrynco@gmail.com>
2017-09-24 13:24:06 +0300

    Added server side annotation validation

--- Sources/TBC-Test.Web.API/Models/PersonModel.cs
+++ Sources/TBC-Test.Web.API/Models/PersonModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using TBC_Test.Business.Domain;
 
 namespace TBC_Test.Web.API.Models
@@ -20,12 +21,25 @@ namespace TBC_Test.Web.API.Models
             Id = person.Id;
         }
 
+        [Required]
         public DateTime Birthdate { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public Gender? Gender { get; set; }
+
         public int Id { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression("^\\d{10}$", ErrorMessage = "Only digits are allowed (10 digits)")]
         public string PersonalNumber { get; set; }
+
+        [Required]
         public decimal Salary { get; set; }
     }
 }
